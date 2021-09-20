a923c9581d5b4a6116e0a7755b7d73c3683e72f4
davi <manelv8@gmail.com>
2021-09-20 16:22:08 -0300

    finish optimizations

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
-import { useEffect, useState } from 'react';
+import { useCallback, useEffect, useState } from 'react';
 
 import { Button } from './components/Button';
+import {SideBar} from './components/SideBar';
 import { MovieCard } from './components/MovieCard';
 
 // import { SideBar } from './components/SideBar';
@@ -12,6 +13,7 @@ import './styles/global.scss';
 
 import './styles/sidebar.scss';
 import './styles/content.scss';
+import { Content } from './components/Content';
 
 interface GenreResponseProps {
   id: number;
@@ -54,41 +56,26 @@ export function App() {
     })
   }, [selectedGenreId]);
 
-  function handleClickButton(id: number) {
-    setSelectedGenreId(id);
-  }
+  const handleClickButton = useCallback(
+      (id: number) => {
+        setSelectedGenreId(id);
+      },[selectedGenreId]
+    ) 
+ 
+  
 
   return (
     <div style={{ display: 'flex', flexDirection: 'row' }}>
-      <nav className="sidebar">
-        <span>Watch<p>Me</p></span>
-
-        <div className="buttons-container">
-          {genres.map(genre => (
-            <Button
-              key={String(genre.id)}
-              title={genre.title}
-              iconName={genre.name}
-              onClick={() => handleClickButton(genre.id)}
-              selected={selectedGenreId === genre.id}
-            />
-          ))}
-        </div>
-
-      </nav>
+     
+      <SideBar 
+        genres={genres} 
+        handleClickButton={handleClickButton} 
+        selectedGenreId={selectedGenreId}/>
 
       <div className="container">
-        <header>
-          <span className="category">Categoria:<span> {selectedGenre.title}</span></span>
-        </header>
-
-        <main>
-          <div className="movies-list">
-            {movies.map(movie => (
-              <MovieCard key ={movie.imdbID} title={movie.Title} poster={movie.Poster} runtime={movie.Runtime} rating={movie.Ratings[0].Value} />
-            ))}
-          </div>
-        </main>
+        <Content 
+          title={selectedGenre.title}
+          movies={movies}/>
       </div>
     </div>
   )
--- src/components/Content.tsx
+++ src/components/Content.tsx
@@ -1,3 +1,43 @@
-export function Content() {
-  // Complete aqui
+import { MovieCard } from "./MovieCard"
+
+
+interface Movie {
+  imdbID: string;
+  Title: string;
+  Poster: string;
+  Ratings: Array<{
+    Source: string;
+    Value: string;
+  }>;
+  Runtime: string;
 }
+interface ContentProps{
+  title: string;
+  movies: Array<Movie>;
+}
+
+export function Content({title, movies}:ContentProps) {
+  return(
+    <>
+      <header>
+        <span className="category">Categoria:<span> {title}</span></span>
+      </header>
+
+      <main>
+        <div className="movies-list">
+          {movies.map(movie => (
+            <MovieCard key ={movie.imdbID} title={movie.Title} poster={movie.Poster} runtime={movie.Runtime} rating={movie.Ratings[0].Value} />
+          ))}
+        </div>
+      </main>
+    </>
+    
+  )
+}
+
+/**
+ * 
+ * 
+ * 
+ * 
+ */
